f7975f293b0508b40805ac7d8711edc81af3f88b
Marko Dojkić <marko.dojkic@gmail.com>
2021-09-17 19:25:06 +0200

    Updated HTTP links, course and index logic and translations. Fixes and testing pending.

--- app/src/main/java/com/example/androidstudiolearning/RegisterActivity.java
+++ app/src/main/java/com/example/androidstudiolearning/RegisterActivity.java
@@ -8,25 +8,33 @@ import android.text.Editable;
 import android.text.InputFilter;
 import android.text.InputType;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
-//import android.widget.Toast;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
 
 import androidx.annotation.ArrayRes;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-//import androidx.biometric.BiometricManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import cn.pedant.SweetAlert.SweetAlertDialog;
+
+//import android.widget.Toast;
+//import androidx.biometric.BiometricManager;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -38,6 +46,8 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText lozinka;
     private Spinner faculties;
     private Spinner courses;
+    private Spinner yearIndex;
+    private List<Integer>   studyId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,62 +58,30 @@ public class RegisterActivity extends AppCompatActivity {
         brIndeksaREG_txt = findViewById(R.id.indexREG_txt);
         studentskaEmail = findViewById(R.id.singiMailREG_txt);
         lozinka = findViewById(R.id.passREG_txt);
-
         faculties = findViewById(R.id.faculty_spin);
         courses = findViewById(R.id.course_spin);
-        brIndeksaREG_txt.setFilters(new InputFilter[] { new InputFilter.LengthFilter(11)});
+        yearIndex = findViewById(R.id.indexYear_spin);
+        studyId = new ArrayList<>();
+
+        ArrayAdapter<Integer> yearAdapter = new ArrayAdapter<Integer>(this, R.layout.support_simple_spinner_dropdown_item, IntStream.rangeClosed(2000, 9999).boxed().collect(Collectors.toList()));
+        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        yearIndex.setAdapter(yearAdapter);
+
+        brIndeksaREG_txt.setFilters(new InputFilter[] { new InputFilter.LengthFilter(6)});
         brIndeksaREG_txt.setInputType(InputType.TYPE_CLASS_NUMBER);
-        brIndeksaREG_txt.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                brIndeksaREG_txt.setOnKeyListener((v, keyCode, event) -> {
-                    if(brIndeksaREG_txt.getText().toString().length() == 5){
-                        if(keyCode == KeyEvent.KEYCODE_DEL){
-                            brIndeksaREG_txt.setText(brIndeksaREG_txt.getText().toString().substring(0,3));
-                            brIndeksaREG_txt.setSelection(3);
-                        }
-                        else {
-                            brIndeksaREG_txt.setText(String.format("%s/%s", brIndeksaREG_txt.getText().toString().substring(0, 4), brIndeksaREG_txt.getText().toString().substring(4, brIndeksaREG_txt.getText().length())));
-                            brIndeksaREG_txt.setSelection(6);
-                        }
-                    }
-                    return false;
-                });
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
 
         studentskaEmail.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                studentskaEmail.setOnKeyListener((v, keyCode, event) -> {
-                    if(studentskaEmail.getText().toString().matches("^[a-z]+\\.[a-z]+\\.[0-9]{2,3}@(singimail.rs)?$")){
-                        if(keyCode == KeyEvent.KEYCODE_DEL){
-                            studentskaEmail.setText(studentskaEmail.getText().toString().substring(0,studentskaEmail.getText().toString().indexOf("@")));
-                            studentskaEmail.setSelection(studentskaEmail.getText().length());
-                        }
-                        else {
-                            studentskaEmail.setText(String.format("%s%s", studentskaEmail.getText().toString(), "singimail.rs"));
-                            studentskaEmail.setSelection(studentskaEmail.getText().length());
-                            studentskaEmail.setFilters(new InputFilter[] {new InputFilter.LengthFilter(studentskaEmail.length())});
-                        }
-                    }
-                    return false;
-                });
+
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
+                if(studentskaEmail.getText().toString().matches("^[a-z]+\\.[a-z]+\\.[0-9]{2,3}@$")){
+                    studentskaEmail.setText(String.format("%s%s", studentskaEmail.getText().subSequence(0, studentskaEmail.getText().toString().indexOf("@") + 1), "singimail.rs"));
+                    studentskaEmail.setSelection(studentskaEmail.getText().toString().indexOf("@"));
+                }
             }
 
             @Override
@@ -115,17 +93,17 @@ public class RegisterActivity extends AppCompatActivity {
         faculties.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                 @ArrayRes int coursesID;
+                 @ArrayRes int facultyID;
 
                 switch (position){
-                    case 0: coursesID = isSerbian ? R.array.courses_f1_srb : R.array.courses_f1_eng; break;
-                    case 1: coursesID = isSerbian ? R.array.courses_f2_srb : R.array.courses_f2_eng; break;
-                    case 2: coursesID = isSerbian ? R.array.courses_f3_srb : R.array.courses_f3_eng; break;
-                    case 3: coursesID = isSerbian ? R.array.courses_f4_srb : R.array.courses_f4_eng; break;
-                    case 4: coursesID = isSerbian ? R.array.courses_f5_srb : R.array.courses_f5_eng; break;
-                    default: coursesID = R.array.courses_f6_srb;
+                    case 0: facultyID = isSerbian ? R.array.courses_f1_srb : R.array.courses_f1_eng; break;
+                    case 1: facultyID = isSerbian ? R.array.courses_f2_srb : R.array.courses_f2_eng; break;
+                    case 2: facultyID = isSerbian ? R.array.courses_f3_srb : R.array.courses_f3_eng; break;
+                    case 3: facultyID = isSerbian ? R.array.courses_f4_srb : R.array.courses_f4_eng; break;
+                    case 4: facultyID = isSerbian ? R.array.courses_f5_srb : R.array.courses_f5_eng; break;
+                    default: facultyID = R.array.courses_f6_srb;
                 }
-                updateCourses(coursesID);
+                updateCourses(facultyID);
             }
 
             @Override
@@ -135,9 +113,24 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    private void updateCourses(@ArrayRes int cID){
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.multiline_simple_spinner, getResources().getStringArray(cID));
+    private void updateCourses(@ArrayRes int fID){
+        studyId.clear();
 
+        switch (fID){
+            case 2130903041: studyId.addAll(List.of(10, 11)); break;
+            case 2130903043: studyId.addAll(List.of(12)); break;
+            case 2130903045: studyId.addAll(List.of(13)); break;
+            case 2130903047: studyId.addAll(List.of(14)); break;
+            case 2130903049: studyId.addAll(List.of(15)); break;
+            case 2130903042: studyId.addAll(List.of(1, 2)); break;
+            case 2130903044: studyId.addAll(List.of(3)); break;
+            case 2130903046: studyId.addAll(List.of(4, 5)); break;
+            case 2130903048: studyId.addAll(List.of(6)); break;
+            case 2130903050: studyId.addAll(List.of(7, 8)); break;
+            default: studyId.addAll(List.of(9, 16)); break;
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.multiline_simple_spinner, getResources().getStringArray(fID));
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         courses.setAdapter(adapter);
     }
@@ -178,7 +171,17 @@ public class RegisterActivity extends AppCompatActivity {
             hasError = false;
         }
 
-        if(brIndeksaREG_txt.getText() == null || brIndeksaREG_txt.getText().length() != 11) {
+        if(yearIndex.getSelectedItem() == null) {
+            yearIndex.setBackgroundColor(Color.RED);
+            hasError = true;
+            return;
+        }
+        else {
+            yearIndex.setBackgroundColor(Color.GREEN);
+            hasError = false;
+        }
+
+        if(brIndeksaREG_txt.getText() == null || brIndeksaREG_txt.getText().length() != 6) {
             brIndeksaREG_txt.setBackgroundColor(Color.RED);
             hasError = true;
             return;
@@ -235,42 +238,34 @@ public class RegisterActivity extends AppCompatActivity {
             }
             else Toast.makeText(RegisterActivity.this, R.string.noFingerprintCapability, Toast.LENGTH_LONG ).show(); //delete this when implemented
             */
-            final SweetAlertDialog waitingDialog = new SweetAlertDialog(RegisterActivity.this, SweetAlertDialog.PROGRESS_TYPE);
-            waitingDialog.getProgressHelper().setBarColor(Color.YELLOW);
-            waitingDialog.setTitleText(R.string.regWaiting);
-            waitingDialog.setCancelable(false);
-            waitingDialog.show();
 
             Thread thread = new Thread(() -> {
                 try {
-                    Socket socket = new Socket("192.168.8.105", 21682);
-                    String response;
-                    BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    PrintWriter output = new PrintWriter(socket.getOutputStream());
+                    URL url = new URL("http://192.168.8.102:62812/api/insert/student");
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("POST");
+                    connection.setRequestProperty("Accept", "application/json;charset=UTF-8");
+                    connection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                    connection.setDoInput(true);
+                    connection.setDoOutput(true);
+                    connection.setConnectTimeout(1000);
 
-                    /*
-                    byte[] decodedKey = Base64.decode("8p6pyFULk8j3DV/yHJaGzw==", Base64.NO_WRAP); //8p6pyFULk8j3DV/yHJaGzw== - is Base64 encoded key (Base64.NO_WRAP)
-                    SecretKey secretKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
-                    Cipher cipher = Cipher.getInstance("AES"); // cipher is not thread safe
+                    OutputStreamWriter output = new OutputStreamWriter(connection.getOutputStream());
 
-                    //ENCODE PASSWORD
-                    cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-                    String encryptedPwd = Base64.encodeToString(cipher.doFinal(lozinka.getText().toString().getBytes(StandardCharsets.UTF_8)), Base64.NO_WRAP);
+                    JSONObject data = new JSONObject();
+                    data.put("name_surname", imePrezime.getText().toString());
+                    data.put("index", yearIndex.getSelectedItem().toString() + "/" + brIndeksaREG_txt.getText().toString());
+                    data.put("password_hash", lozinka.getText().toString());
+                    data.put("email", studentskaEmail.getText().toString());
+                    data.put("studyId", studyId.get(courses.getSelectedItemPosition()));
+                    data.put("year", String.valueOf(Math.round(Math.ceil(Math.random()*( studyId.get(courses.getSelectedItemPosition()) == 16 ? 5 : 4 ))))); //farmacija traje 5 godina
 
-                    // DECODE encryptedPwd String
-                    cipher.init(Cipher.DECRYPT_MODE, secretKey);
-                    String decryptedPwd = new String(cipher.doFinal(Base64.decode(encryptedPwd, Base64.NO_WRAP)), "UTF-8");
-                    EXAMPLE: jQcRAV7wi/a4JWZekAwP3w== = Mar****99 - EXAMPLE OF ENCRYPTION USING KEY ABOVE
-                    */
+                    connection.connect();
+                    output.write(data.toString());
+                    output.flush();
+                    output.close();
 
-                    output.print(imePrezime.getText().toString() + "*" + brIndeksaREG_txt.getText().toString() + "*" + studentskaEmail.getText().toString() + "*" + lozinka.getText().toString() + "*" + faculties.getSelectedItem().toString() + "*" + courses.getSelectedItem().toString());
-                    output.flush(); // * is delimiter
-                    //SOCKET ACCESS DENIED - SOLUTION : PERMISSION FROM INTERNET TO MANIFEST
-
-                    socket.setSoTimeout(0); //wait for approval
-                    response = input.readLine();
-                    if(response.contains("APPROVED")){
-                        waitingDialog.dismiss();
+                    if(connection.getResponseCode() == 200){
                         RegisterActivity.this.runOnUiThread(() -> {
                             final SweetAlertDialog successDialog = new SweetAlertDialog(RegisterActivity.this, SweetAlertDialog.SUCCESS_TYPE);
                             successDialog
@@ -283,10 +278,8 @@ public class RegisterActivity extends AppCompatActivity {
                                     })
                                     .show();
                         });
-
                     }
-                    else if(response.contains("DENIED")){
-                        waitingDialog.dismiss();
+                    else if(connection.getResponseCode() == 500){
                         RegisterActivity.this.runOnUiThread(() -> {
                             final SweetAlertDialog failedDialog = new SweetAlertDialog(RegisterActivity.this, SweetAlertDialog.ERROR_TYPE);
                             failedDialog
@@ -297,12 +290,30 @@ public class RegisterActivity extends AppCompatActivity {
                                     .show();
                         });
                     }
-                    output.write("END"); //END CONNECTION ON SERVER SIDE
-                    output.flush();
-                    input.close();
-                    output.close();
-                    socket.close();
-                } catch (IOException e) {
+                    else {
+                        RegisterActivity.this.runOnUiThread(() -> {
+                            final SweetAlertDialog failedDialog = new SweetAlertDialog(RegisterActivity.this, SweetAlertDialog.WARNING_TYPE);
+                            failedDialog
+                                    .setTitleText(R.string.regTitleFailed)
+                                    .setContentText(getResources().getString(R.string.regMessageServerError))
+                                    .setConfirmText(getResources().getString(R.string.ok))
+                                    .setConfirmClickListener(sweetAlertDialog -> failedDialog.dismiss())
+                                    .show();
+                        });
+                    }
+
+                    connection.disconnect();
+                } catch (IOException | JSONException e) {
+                    if(e.getClass().getName().equals(SocketTimeoutException.class.getName())){
+                        RegisterActivity.this.runOnUiThread(() -> {
+                            final SweetAlertDialog warningDialog = new SweetAlertDialog(RegisterActivity.this, SweetAlertDialog.WARNING_TYPE);
+                            warningDialog
+                                    .setTitleText(R.string.regTitleFailed)
+                                    .setConfirmText(getResources().getString(R.string.regMessageServerError))
+                                    .setConfirmText(getResources().getString(R.string.ok))
+                                    .show();
+                        });
+                    }
                     e.printStackTrace();
                 }
             });
